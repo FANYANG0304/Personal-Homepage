21b1cabf5df580182d6b5563f02bc1ba637c82d9
FANYANG0304 <yangfann@upenn.edu>
2026-02-05 20:09:50 -0500

    0205photo

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/Personal-Homepage',
+  assetPrefix: '/Personal-Homepage',
   trailingSlash: true,
   images: {
     unoptimized: true,

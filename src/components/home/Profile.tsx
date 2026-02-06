ea202d4e807b86fc4ee4506c8f64e718ebd2105c
FANYANG0304 <yangfann@upenn.edu>
2026-02-05 20:17:58 -0500

    0205photo2

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -112,7 +112,7 @@ export default function Profile({ author, social, features, researchInterests }:
             {/* Profile Image */}
             <div className="w-64 h-64 mx-auto mb-6 rounded-2xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105">
                 <Image
-                    src={author.avatar}
+                    src={`/Personal-Homepage${author.avatar}`}
                     alt={author.name}
                     width={256}
                     height={256}
